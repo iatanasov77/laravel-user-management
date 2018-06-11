5ea221e43618e202a01de37df2959cc2fa534e55
Ivan I. Atanasov <ia@vankosoft.org>
2018-06-11 14:51:07 +0300

    Fix routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,22 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Request;
+Route::group( ['middleware' => 'web', 'prefix' => 'users', 'namespace' => 'IA\Laravel\Modules\UserManagement\Http\Controllers'], function()
+{
 
-Route::group( ['prefix' => Request::segment( 1 )], function () {
-    Route::group( ['middleware' => 'web', 'prefix' => 'admin', 'namespace' => 'IA\Laravel\Modules\UserManagement\Http\Controllers'], function()
-    {
+    //Route::model( 'users', 'IA\Laravel\Modules\UserManagement\Entity\User');
+    Route::resource( 'users', 'UsersController', [ 'as' => 'admin.users'] );
 
-        //Route::model( 'users', 'IA\Laravel\Modules\UserManagement\Entity\User');
-        Route::resource( 'users/users', 'UsersController', [ 'as' => 'admin.users'] );
+    //Route::model( 'roles', 'IA\Laravel\Modules\UserManagement\Entity\Role');
+    Route::resource( 'roles', 'RolesController', [ 'as' => 'admin.users'] );
 
-        //Route::model( 'roles', 'IA\Laravel\Modules\UserManagement\Entity\Role');
-        Route::resource( 'users/roles', 'RolesController', [ 'as' => 'admin.users'] );
+    Route::get( 'permissions/json_tree/{roleId}', 'PermissionsController@jsonTree' )
+        ->name( 'admin.users.permissions.json-tree' );
 
-        Route::get( 'permissions/json_tree/{roleId}', 'PermissionsController@jsonTree' )
-            ->name( 'admin.users.permissions.json-tree' );
-
-        // Profile
-        Route::get( 'profile', 'ProfileController@showForm' )   ->name( 'admin.users.profile.show-form' );
-        Route::patch( 'profile', 'ProfileController@save' )     ->name( 'admin.users.profile.save' );
-    });
+    // Profile
+    Route::get( 'profile', 'ProfileController@showForm' )   ->name( 'admin.users.profile.show-form' );
+    Route::patch( 'profile', 'ProfileController@save' )     ->name( 'admin.users.profile.save' );
 });
